5faa43e7e26edf498206cc917559dbede462e3f5
Omafovbe I. <i.omafovbe@gmail.com>
2025-02-17 22:45:11 +0100

    stripe api-version in webhhook

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe'
 import { handleSubscriptionChange } from '@/actions/stripe-actions'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-01-27.acacia',
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
